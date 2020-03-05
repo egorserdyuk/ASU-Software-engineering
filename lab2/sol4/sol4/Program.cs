fc20ef78959d97381137f4578e63dba6b5f06176
Egor Serdyuk <egor.serduck@gmail.com>
2020-03-05 17:26:48 +0700

    Solution for task 4 in lab 2

---
+++ lab2/sol4/sol4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab2sol4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 100; i < 1000; i++)
+            {
+                int[] mas = { i / 100, i % 100 };
+                if (mas[0] == mas[1])
+                    Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
