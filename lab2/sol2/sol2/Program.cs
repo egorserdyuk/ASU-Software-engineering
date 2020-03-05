5bd95aa61aea45bb879908c5adb4afde33186f80
Egor Serdyuk <egor.serduck@gmail.com>
2020-03-05 17:17:41 +0700

    Solution for task 2 in lab 2

---
+++ lab2/sol2/sol2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+
+namespace lab2sol2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            Console.Write("x = ");
+            x = double.Parse(Console.ReadLine());
+            Console.Write("y = ");
+            y = double.Parse(Console.ReadLine());
+
+            const int R = 10;
+
+            if ((x * x + y * y < R * R) && (y > -x)) Console.WriteLine("Yes");
+            else if (x * x + y * y > R * R) Console.WriteLine("Not");
+            else Console.WriteLine("On the board");
+
+            Console.ReadKey();
+        }
+    }
+}
