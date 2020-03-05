3e6c2cd1ff2c1501bb9f5679b478b9a3a2adc216
Egor Serdyuk <egor.serduck@gmail.com>
2020-03-05 17:15:08 +0700

    Solution for task 1 in lab 2

---
+++ lab2/sol1/sol1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("x = ");
+            float x = float.Parse(Console.ReadLine());
+            double y = (3 / Math.Abs(Math.Pow(x, 3) + 8));
+            Console.Write("y = 3/|x^3 + 8| = {0}", y);
+
+            Console.ReadKey();
+        }
+    }
+}
