1552a19e3e090e5930b9ab1a0a256cdf86edb4ab
Egor Serdyuk <egor.serduck@gmail.com>
2020-03-05 17:25:14 +0700

    Solution for task 3 in lab 2

---
+++ lab2/sol3/sol3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2sol3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char gender;
+            do
+            {
+                Console.Write("Enter gender to see example names: ");
+                gender = char.Parse(Console.ReadLine());
+                switch (gender)
+                {
+                    case 'm':
+                        Console.Write("Peter, John, George\n");
+                        break;
+                    case 'f':
+                        Console.Write("Eva, Victoria, Julia\n");
+                        break;
+                    case 'e':
+                        break;
+                    default:
+                        Console.Write("There is only 2 genders\n");
+                        break;
+                }
+            } while (gender != 'e');
+
+            Console.ReadKey();
+        }
+    }
+}
