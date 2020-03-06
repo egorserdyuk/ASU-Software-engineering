21078019af63fd92d8aff5dfb7127b686e9eb10b
Egor Serdyuk <egor.serduck@gmail.com>
2020-03-06 14:56:21 +0700

    Update Program.cs

--- lab2/sol5/sol5/Program.cs
+++ lab2/sol5/sol5/Program.cs
@@ -14,17 +14,17 @@ namespace lab2sol5
 
             for (int i = 0; i <= n; ++i)
             {
-                for (int j = i; j >= 1; --j)
+                for (int j = i; j > 0; --j)
                 {
                     if (j == 1) Console.Write("{0, 4}", 3);
                     else Console.Write("{0, 4}", 2);
                 }
                 Console.WriteLine();
 
-                for (int j = i; j >= 1; --j)
+                for (int j = i; j > 0; --j)
                 {
                     if (j == 1) Console.Write("{0, 4}", 0);
-                    else Console.Write("{0, 4}", (10 - j));
+                    else Console.Write("{0, 4}", (11 - j));
                 }
                 Console.WriteLine();
 
