2b6a7d4111e9ddc86e1f1b2862ca204b55b17a65
Egor Serdyuk <egor.serduck@gmail.com>
2020-03-19 02:33:24 +0700

    Some kind solution 2 for task 4

---
+++ lab4/sol2/sol2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace sol2
+{
+    class Program
+    {
+        static void delNums(StringBuilder str)
+        {
+            for (int i = 0; i < str.Length; i++)    //Проходим по строке и если
+            {
+                if (char.IsDigit(str[i]))
+                {
+                    str.Remove(i, 1);
+                    i -= 2;
+                }
+            }
+
+            Console.WriteLine("New line: {0}", str);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string str = Console.ReadLine();
+
+            StringBuilder myStr = new StringBuilder(str);
+
+            delNums(myStr);
+            Console.WriteLine(myStr);
+        }
+    }
+}
