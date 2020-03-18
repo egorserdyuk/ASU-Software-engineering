334f7095b12c5ad92b0f30abaee76b979e5a1d68
Egor Serdyuk <egor.serduck@gmail.com>
2020-03-19 03:08:10 +0700

    solution 3 for task 4

---
+++ lab4/sol3/sol3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace sol3
+{
+    class Program
+    {
+
+        static void alphabetSort(string str)
+        {
+            char[] splitter = { ' ', ',', '.', ';', ':', '?' };
+
+            //String[] words = str.Split(splitter);
+
+            Array.Sort(str, StringComparer.InvariantCulture);
+
+            foreach (string item in str)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Input string: ");
+            string str = Console.ReadLine();
+
+            StringBuilder myStr = new StringBuilder(str);
+
+            alphabetSort(str);
+        }
+    }
+}
