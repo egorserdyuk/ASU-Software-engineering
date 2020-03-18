5cb09d22cc3b5e5ae8511c232094c21e79a464b8
Egor Serdyuk <egor.serduck@gmail.com>
2020-03-19 02:05:55 +0700

    Update Program.cs

--- lab4/sol1/sol1/Program.cs
+++ lab4/sol1/sol1/Program.cs
@@ -4,9 +4,34 @@ namespace sol1
 {
     class Program
     {
+        static void beforeColon(string str) //Вывод символов после двоеточия
+        {
+            bool flag = false; //Если нет двоеточий
+            for (int x = 0; x < str.Length; x++) //Находим где стоит первое двоеточие
+            {
+                if (str[x] == ':') //Выбираем символы до первого двоеточия
+                {
+                    flag = true;
+                    Console.Write("Characters before the first colon: ");
+                    for (int i = 0; i < x; i++)
+                        Console.Write(str[i]);
+                    break;
+                }
+            }
+
+            if (!flag)
+                Console.Write("There are no colons in the line");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Input string: ");
+            string str = Console.ReadLine();
+
+            beforeColon(str);
+
+            //Console.WriteLine(str.Split(':')[0]); //Хех, это тоже работает стабильно
         }
+
     }
 }
