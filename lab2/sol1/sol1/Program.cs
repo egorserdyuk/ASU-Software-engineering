997827896d9afdf2cd2b91a61173a06764ae5fdb
Egor Serdyuk <egor.serduck@gmail.com>
2020-04-01 00:29:52 +0700

    Update Program.cs

--- lab2/sol1/sol1/Program.cs
+++ lab2/sol1/sol1/Program.cs
@@ -10,10 +10,22 @@ namespace lab2
     {
         static void Main(string[] args)
         {
-            Console.Write("x = ");
-            float x = float.Parse(Console.ReadLine());
-            double y = (3 / Math.Abs(Math.Pow(x, 3) + 8));
-            Console.Write("y = 3/|x^3 + 8| = {0}", y);
+            
+            do {
+                Console.Write("x = ");
+                float x = float.Parse(Console.ReadLine());
+
+                if (x == -2 || x == (2 * Math.Pow(-1, 1.0 / 3)) || x == (-2 * Math.Pow(-1, 2/3))) // Проверка условий, иначе выдаст бесконечность
+                {
+                    Console.Write("Error, try again\n");
+                }
+                else // Если все ок - считаем по условию
+                {
+                    double y = (3 / Math.Abs(Math.Pow(x, 3) + 8));
+                    Console.Write("y = 3/|x^3 + 8| = {0}", y); // Выводим результат
+                    break;
+                }
+            } while(true) ;
 
             Console.ReadKey();
         }
