4b9b1da4c4e63c77407f63cb0cca55966863ad24
Egor Serdyuk <egor.serduck@gmail.com>
2020-04-01 00:36:33 +0700

    Update Program.cs

--- lab2/sol5/sol5/Program.cs
+++ lab2/sol5/sol5/Program.cs
@@ -10,18 +10,18 @@ namespace lab2sol5
         static void Main(string[] args)
         {
             Console.Write("n = ");
-            int n = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine()); // Сколько раз нужно вывести
 
             for (int i = 0; i <= n; ++i)
             {
-                for (int j = i; j > 0; --j)
+                for (int j = i; j > 0; --j) // Выводим верхний ряд 3 и 2
                 {
                     if (j == 1) Console.Write("{0, 4}", 3);
                     else Console.Write("{0, 4}", 2);
                 }
                 Console.WriteLine();
 
-                for (int j = i; j > 0; --j)
+                for (int j = i; j > 0; --j) // Выводим по закономерности
                 {
                     if (j == 1) Console.Write("{0, 4}", 0);
                     else Console.Write("{0, 4}", (11 - j));
