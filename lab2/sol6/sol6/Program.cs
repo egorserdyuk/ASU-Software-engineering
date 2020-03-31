67431a21b0870f25c128a609d1c55b0d0116a480
Egor Serdyuk <egor.serduck@gmail.com>
2020-04-01 00:37:06 +0700

    Update Program.cs

--- lab2/sol6/sol6/Program.cs
+++ lab2/sol6/sol6/Program.cs
@@ -19,7 +19,7 @@ namespace ConsoleApplication1
             h = Convert.ToDouble(Console.ReadLine()); //шаг
 
             Console.WriteLine("{0,3} {1,5} {2,5}", "#", "x", "f(x)");
-            for (double x = a; x <= b; x += h, ++i) //цикл от а до b шагом h
+            for (double x = a; x <= b; x += h, ++i) //цикл от а до b шагом h по условию задачи
             {
                 if (x < 1)
                     y = Math.Pow((Math.Pow(x, 2) - 1), 2);
@@ -27,7 +27,7 @@ namespace ConsoleApplication1
                     y = 1 / (Math.Pow((1 + x), 2));
                 else
                     y = 0;
-                Console.WriteLine("{0,3} {1,5:f2} {2,5:f2}", i, x, y);
+                Console.WriteLine("{0,3} {1,5:f2} {2,5:f2}", i, x, y); //вывод таблицы
             }
 			
 			Console.ReadKey();
