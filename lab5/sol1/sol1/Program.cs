ceedb9dcf8af5db32e8bc1d6ef57a18b2f0acb3e
Egor Serdyuk <egor.serduck@gmail.com>
2020-04-01 22:37:11 +0700

    Solution 1 for task 5
    Have some ideas but it doesn't work... that's ok

---
+++ lab5/sol1/sol1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace sol1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input string: ");
+            string str = Console.ReadLine();
+
+            Regex RegExp = new Regex(@"\d+");
+
+            MatchCollection matches = RegExp.Matches(str);
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                    Console.WriteLine("Result = {0}", match.Value);
+            }
+            else
+            {
+                Console.WriteLine("Error, haven't such expressions");
+            }
+        }
+    }
+}
