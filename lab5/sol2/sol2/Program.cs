ac7efc534828547e3870ef14413a7920138e740b
Egor Serdyuk <egor.serduck@gmail.com>
2020-04-20 11:49:21 +0700

    FINAL

--- lab5/sol2/sol2/Program.cs
+++ lab5/sol2/sol2/Program.cs
@@ -7,20 +7,28 @@ namespace sol2
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Input string: ");
-            //string str = Console.ReadLine();
-            string str = "22 UDD, 0.002 USD, 200 RUR, 32.21 USD, 666 JPN, 10 EU, 10 CHF, 0.5 RUR";
+            Console.WriteLine("Input string: ");
+            string str = Console.ReadLine();
+            //string str = "100 USD, 22 UDD, 0.002 USD, 200 RUR, 32.21 USD, 666 JPN, 10 EU, 10 CHF, 0.5 RUR"; // На чем проверял
 
-            Regex RegExp = new Regex(@"(((\d+))|((\d+\.\d{1,2}?)))\s*(?:USD|EU|RUR)");
+            Regex RegExp = new Regex(@"((\s\d+)|(\A(\d+))|(\d+\.\d{1,2}))\s(?:USD|EU|RUR)"); // Тут мы ищем числа, которые: с точкой, но не более 2 символов; целые; все они должны быть подписаны валютой EU, RUR или USD
+            MatchCollection matchcollection = RegExp.Matches(str);
 
-            MatchCollection matches = RegExp.Matches(str);
-            if (matches.Count > 0)
-            {
-                foreach (Match match in matches)
-                    Console.WriteLine("Result = {0}", match.Value);
+            string text = "";
+            string textEnd = "";
+
+            MatchCollection matches1 = RegExp.Matches(str);
+            if (matches1.Count > 0) {
+                for (int i = 0; i < matches1.Count; i++)
+                {
+                    text += matchcollection[i].Value + " ";
+                }
+
+                textEnd = System.Text.RegularExpressions.Regex.Replace(text, @"\s+", " "); // Чистим текст перестановкой по регулярке (против лишних пробелов)
+
+                Console.WriteLine("Numbers are: {0}", textEnd);
             }
-            else
-            {
+            else {
                 Console.WriteLine("Error, haven't such expressions");
             }
         }
