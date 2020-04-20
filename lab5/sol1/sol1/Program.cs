181f0f551990fc51d97a6d95d822c7f9488f91df
Egor Serdyuk <egor.serduck@gmail.com>
2020-04-20 11:50:58 +0700

    FINAL

--- lab5/sol1/sol1/Program.cs
+++ lab5/sol1/sol1/Program.cs
@@ -12,7 +12,7 @@ namespace sol1
             string str = Console.ReadLine();
             //string str = "6000 7000 3.99 200 or 1337,42 EXAMPLE; 1 EXAMPLE| 520.5969 42 43 320";
 
-            Regex RegExp = new Regex(@"\d+(?!\d+[.,]?\d*)"); // Каждое значение с цифрами не может содержать значения с "," или "."; остальные - ок
+            Regex RegExp = new Regex(@"\d+(?!\d+[.,]?\d*)"); // Каждое значение с цифрами не может содержать значения с "," или "."; остальные - ок, потому что целые
 
             int result = RegExp.Matches(str).Cast<Match>().Max(x => int.Parse(x.ToString())); // Преобразуем Match'и в список и находим максимум
             Console.WriteLine(result);
