72cfb529373f8db2072265769943ffea52df3374
Egor Serdyuk <egor.serduck@gmail.com>
2020-04-28 11:41:54 +0700

    don't look at it

---
+++ lab6/sol1/sol1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace sol1
+{
+    public class String
+    {
+        private int length;
+        public String(int len = 0)
+        {
+            length = len;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
