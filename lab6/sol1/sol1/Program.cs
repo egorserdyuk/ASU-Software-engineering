434e98f90097141c9bdc5e15521bb12e1f44cc76
Egor Serdyuk <egor.serduck@gmail.com>
2020-04-30 00:15:50 +0700

    Update Program.cs

--- lab6/sol1/sol1/Program.cs
+++ lab6/sol1/sol1/Program.cs
@@ -20,7 +20,43 @@ namespace sol1
                 set => data = value;
             }
 
-            
+            public double ToMyDouble()
+            {
+                string temp = "";
+                double outDouble = 0;
+
+                //for (int i = 0; i < length; i++)
+                //{
+                //    outDouble += Convert.ToDouble(data[i]);
+                //}
+                foreach (int i in data)
+                {
+                    // Using ToString() method 
+                    temp += i.ToString();
+                }
+                double.TryParse(temp, out outDouble);
+
+                return outDouble;
+            }
+
+            public double ToMyString()
+            {
+                string temp = "";
+                MyString str;
+                double num = 0;
+
+                //temp = Convert.ToString(data);
+                for (int i = 0; i < length; i++)
+                {
+                    // Using ToString() method 
+                    temp += data[i].ToString();
+                }
+                Console.WriteLine("\n{0}\n", temp);
+
+                num = Convert.ToDouble(temp);
+                //double.TryParse(temp, out num);
+                return num;
+            }
 
             public MyString(bool rand = false, int length = 10)
             {
@@ -90,13 +126,15 @@ namespace sol1
 
             static void Main()
             {
-                MyString str1 = new MyString(true, 10); //создаем объект класса
-                MyString str2 = new MyString(str1, 5); //создаем копию объекта класса, все элементы больше на 2
+                MyString str1 = new MyString(false, 4); //создаем объект класса
+                MyString str2 = new MyString(str1, 4); //создаем копию объекта класса, все элементы больше на 2
                 str1.output();
                 Console.WriteLine();
                 str2.output(); //выводим объект и его копию
+                double myDouble = str1.ToMyDouble();
+                Console.WriteLine("\nDouble = {0}\n", myDouble);
+                //MyString myString = str2.ToMyString();
                 Console.ReadKey();
-
             }
         }
     }
