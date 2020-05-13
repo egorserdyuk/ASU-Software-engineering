156a2e8a2f3f5dc4da91fe394583374162ccce5e
Egor Serdyuk <egor.serduck@gmail.com>
2020-05-13 12:36:44 +0700

    Update Program.cs
    FINAL version

--- lab6/sol1/sol1/Program.cs
+++ lab6/sol1/sol1/Program.cs
@@ -1,141 +1,130 @@
 ﻿using System;
-
-namespace sol1
+public class ObjectString
 {
-    class Program
+    private int length;  // Length of the string
+    public char[] str;  // String
+
+    public int Length  // Properties
     {
-        class MyString
+        get => length;
+        set => length = value;
+    }
+
+    public ObjectString(int length = 6)  // Default constructor
+    {
+        this.length = length;
+        str = new char[length];
+
+       // Manual input
+       //     do  // If an invalid variable is entered -> the input starts again
+       //     {
+       //         try
+       //         {
+       //             Console.Write("Input the string:\n");
+       //             for (int i = 0; i < length; i++)
+       //             {
+       //                 Console.Write("String[{0}]: ", i);
+       //                 str[i] = Convert.ToChar(Console.ReadLine());
+       //             }
+       //         }
+       //         catch
+       //         {
+       //             Console.Write("Error, the wrong type of variable\n");
+       //             continue;
+       //         }
+       //
+       //         flag = true;
+       //     } while (flag != true);
+        
+        // Random input
+        Random rnd = new Random();
+        for (int i = 0; i < length; i++)
         {
-            private int length; // Длина строки
-            public char[] data;
-
-            public int Length //свойства
-            {
-                get => length;
-                set => length = value;
-            }
-            public char[] Data
-            {
-                get => data;
-                set => data = value;
-            }
-
-            public double ToMyDouble()
-            {
-                string temp = "";
-                double outDouble = 0;
-
-                //for (int i = 0; i < length; i++)
-                //{
-                //    outDouble += Convert.ToDouble(data[i]);
-                //}
-                foreach (int i in data)
-                {
-                    // Using ToString() method 
-                    temp += i.ToString();
-                }
-                double.TryParse(temp, out outDouble);
-
-                return outDouble;
-            }
-
-            public double ToMyString()
-            {
-                string temp = "";
-                MyString str;
-                double num = 0;
-
-                //temp = Convert.ToString(data);
-                for (int i = 0; i < length; i++)
-                {
-                    // Using ToString() method 
-                    temp += data[i].ToString();
-                }
-                Console.WriteLine("\n{0}\n", temp);
-
-                num = Convert.ToDouble(temp);
-                //double.TryParse(temp, out num);
-                return num;
-            }
-
-            public MyString(bool rand = false, int length = 10)
-            {
-                this.length = length;
-
-                bool flag = false;
-                data = new char[length];
-
-                if (rand == false) //ввод с клавиатуры
-                {
-                    do //если введена некорректная переменная, то начинается ввод заного
-                    {
-                        try //ловим исключение
-                        {
-                            Console.Write("Input the string: ");
-                            for (int i = 0; i < length; i++)
-                            {
-                                Console.Write("String: [{0}] : ", i);
-                                data[i] = Convert.ToChar(Console.ReadLine());
-                            }
-                        }
-                        catch
-                        {
-                            Console.Write("Введена переменная не того типа\n");
-                            continue; //все плохо - начинаем новую итерацию
-                        }
-
-                        flag = true; //если все ок, то выходим из цикла
-                    } while (flag != true);
-                }
-                else //рандомный ввод
-                {
-                    Random rnd = new Random();
-                    for (int i = 0; i < length; i++)
-                    {
-                        data[i] = Convert.ToChar(rnd.Next('a', 'a' + 26)); //рандомный символ от 'a' до 'z'
-                    }
-                }
-
-            }
-
-            public MyString(MyString exData, int n = 5) //конструктор копирования. n - сколько прибавить к симыолу
-            {
-                length = exData.length;
-
-                data = new char[length];
-
-                for (int i = 0; i < n; i++)
-                {
-                    data[i] = exData.data[i];
-                }
-            }
-
-            ~MyString() => Console.WriteLine("Сработал деструктор");
-            //сборщик мусора сделает всю работу и нет необходимости явно освобождать память
-
-            public void output() //вывод матрицы
-            {
-                Console.Write("String: ");
-                for (int i = 0; i < length; i++)
-                {
-                    Console.Write("{0}", data[i]);
-
-                }
-
-            }
-
-            static void Main()
-            {
-                MyString str1 = new MyString(false, 4); //создаем объект класса
-                MyString str2 = new MyString(str1, 4); //создаем копию объекта класса, все элементы больше на 2
-                str1.output();
-                Console.WriteLine();
-                str2.output(); //выводим объект и его копию
-                double myDouble = str1.ToMyDouble();
-                Console.WriteLine("\nDouble = {0}\n", myDouble);
-                //MyString myString = str2.ToMyString();
-                Console.ReadKey();
-            }
+            str[i] = Convert.ToChar(rnd.Next('a', 'a' + 26));  // Random character from the english alphabet
         }
+        
+
+    }
+
+    // Copy constructor
+    public ObjectString(ObjectString objectStr, int n = 5)
+    {
+        length = objectStr.length;
+        str = new char[length];
+        for (int i = 0; i < n; i++)
+        {
+            str[i] = objectStr.str[i];
+        }
+    }
+
+    // Destructor
+    ~ObjectString() { }
+
+    // Transforming ObjectString to Double
+    public double[] ToDouble(ObjectString objstr)
+    {
+        double[] doubleStr; // new double string
+        doubleStr = new double[objstr.length];
+        for (int i = 0; i < objstr.length; i++)
+        {
+            doubleStr[i] = str[i];
+        }
+        return doubleStr;
+    }
+
+    // Transformig Double to ObjectString
+    public ObjectString FromDouble(double[] doubleStr)
+    {
+        ObjectString String = new ObjectString(length);
+        for (int i = 0; i < doubleStr.Length; i++)
+        {
+            String.str[i] = (char)doubleStr[i];
+        }
+        return String;
+    }
+
+    // Output method
+    public void output()
+    {
+        Console.WriteLine();
+        for (int i = 0; i < length; i++)
+        {
+            Console.Write("{0}", str[i]);
+        }
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        // create objString
+        ObjectString str1 = new ObjectString();
+        ObjectString str2 = new ObjectString(str1);
+
+        // output the strings
+        Console.Write("\nstr 1: ");
+        str1.output();
+
+        Console.Write("\nstr 2: ");
+        str2.output();
+
+        // double array init
+        double[] doubleStr;
+        doubleStr = new double[str1.Length];  // Especially for str1
+
+        // transformed ObjString str1
+        doubleStr = str1.ToDouble(str1);
+
+        // output double array
+        Console.WriteLine("\nstr1 to Double: ");
+        for (int i = 0; i < doubleStr.Length; i++)
+            Console.Write("{0} ", doubleStr[i]);
+
+        ObjectString str3 = new ObjectString();
+
+        // transformed from double back to ObjString
+        str3 = str3.FromDouble(doubleStr);
+        Console.Write("\nDouble to str3: ");
+        str3.output();
     }
 }
